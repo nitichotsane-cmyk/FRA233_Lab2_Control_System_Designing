a1ce6dc98b969d331fd9c1061cfee8a3f17c9028
Phakin Boonchanachai <pboonchanachai@gmail.com>
2026-01-10 12:57:03 +0800

    Update files

--- Lab2_params_student.m
+++ Lab2_params_student.m
@@ -1,10 +1,8 @@
-%% Pendulum Param
-% ค่าที่ตั้งไว้เป้นค่าสูงสุดที่มอเตอร์สามารถหมุนได้โดยไม่เกิน 12V้
+% Pendulum Param
 L = 0.1;     % [m]  (massless link)
 mp = 0.05;   % [kg] (point mass)
 g = 9.81;    % [m/s^2]
-%% Motor Param 
-% นำค่าที่ได้จาก lab1 มาใส่
+% Motor Param from experiment
 kt = 0.05;
 ke = 0.05;
 Lm = 4e-3;
