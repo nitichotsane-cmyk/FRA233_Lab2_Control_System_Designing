83ff255b4b3bd7c1bf68bdf7d01c27977c57e8db
Nitichot <nitichot.sane@mail.kmutt.ac.th>
2026-03-04 16:35:04 +0700

    new parameter

--- Lab2_params_student.m
+++ Lab2_params_student.m
@@ -3,12 +3,20 @@ L = 0.1;     % [m]  (massless link)
 mp = 0.05;   % [kg] (point mass)
 g = 9.81;    % [m/s^2]
 % Motor Param from experiment
-Eff = 0.9994;
-ke = 0.05072;
-kt = Eff*ke;
-Lm = 0.00327239;
-R = 3.74;
-b = 0.000031521;
-J = 0.000011211;
+%% LAB 1 parameter %%
+% Eff = 0.9994;
+% ke = 0.05072;
+% kt = Eff*ke;
+% Lm = 0.00327239;
+% R = 3.74;
+% b = 0.000031521;
+% J = 0.000011211;
+%% Give Parameter %%
+% Eff = 0.9994;
+ke = 52.8e-3;
+kt = 50.6e-3;
+Lm = 2.8445e-3;
+R = 3.18;
+b = 77.581e-6;
+J = 58.559e-6;
 T = Lm/R;
-
